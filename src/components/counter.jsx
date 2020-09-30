18a223f79a93767ff67da2f24286f6a9dba785af
Matthew Jones <jonesmat@atrihub.io>
2020-09-30 14:14:42 -0700

    feat (MJ React YT): basic input
    Capable of handling basic input to increment an integer value
    
    [ICTRAINING-513]

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,19 +3,26 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    tags: ["tag1", "tag2", "tag3"],
+  };
+
+  constructor() {
+    super();
+  }
+
+  handleIncrement = (product) => {
+    this.setState({ count: this.state.count + 1 });
   };
 
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {this.state.tags.map((tag) => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
+        <button
+          onClick={() => this.handleIncrement(1)}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
       </div>
     );
   }
