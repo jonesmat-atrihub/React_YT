84eb45ead88aa418e21636b7192506ad4af62728
Matthew Jones <jonesmat@atrihub.io>
2020-09-30 14:45:38 -0700

    feat (MJ React YT): onDelete
    Implemented onDelete to send signal from child to parent component
    
    [ICTRAINING-515]

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,26 +2,28 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.value,
   };
 
-  constructor() {
-    super();
-  }
-
-  handleIncrement = (product) => {
-    this.setState({ count: this.state.count + 1 });
+  handleIncrement = () => {
+    this.setState({ value: this.state.value + 1 });
   };
 
   render() {
     return (
       <div>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <span className={this.getBadgeClasses()}>{this.formatvalue()}</span>
         <button
-          onClick={() => this.handleIncrement(1)}
+          onClick={this.handleIncrement}
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>
+        <button
+          onClick={this.props.onDelete}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
       </div>
     );
@@ -29,13 +31,13 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+  formatvalue() {
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
   }
 }
 
