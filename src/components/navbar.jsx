28f4f37580b384a2568bfc5552eabe4eafd11c31
Matthew Jones <jonesmat@atrihub.io>
2020-10-05 10:59:18 -0700

    Fix errors

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,11 @@
-import React, { Component } from "react";
+import React from "react";
 
 //Stateless functional component
 const Navbar = ({ totalCounters }) => {
   console.log("Navbar rendered");
   return (
     <nav className="navbar navbar-light bg-light">
-      <a className="navbar-brand" href="#">
+      <a className="navbar-brand" href="/">
         Navbar{" "}
         <span className="badge badge-pill badge-secondary">
           {totalCounters}
